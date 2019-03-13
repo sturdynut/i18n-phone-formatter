6af4d0a42959076b315e5e91ce44af1f35ad8b45
Matti Salokangas <matti.salokangas@gmail.com>
2019-03-12 20:48:54 -0700

    Uglify bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const umd = require('gulp-umd');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
   return gulp.src('./src/index.js')
     .pipe(babel())
     .pipe(umd())
+    .pipe(uglify())
     .pipe(gulp.dest('dist'));
 });
