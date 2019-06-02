a936c3b233f91bceb4d72c715a6d53e49282acd1
Matti Salokangas <matti.salokangas@gmail.com>
2019-06-01 17:04:02 -0700

    Build and add coverage

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,20 @@
 import { terser } from "rollup-plugin-terser";
 
-
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'umd',
-    name: 'i18nPhoneFormatter'
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    plugins: [ terser() ]
   },
-  plugins: [ terser() ]
-};
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.es6.js',
+      format: 'esm',
+    },
+    plugins: [ terser() ]
+  },
+];
